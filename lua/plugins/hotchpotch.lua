fefb8cee346e477217343e0407cceb0c94ae6fe4
Petyo Ivanov <underlog@gmail.com>
2023-04-18 07:48:34 +0300

    Switch tabs with <Leader>n

--- lua/plugins/hotchpotch.lua
+++ lua/plugins/hotchpotch.lua
@@ -135,4 +135,18 @@ return {
       })
     end,
   },
+  {
+    "akinsho/bufferline.nvim",
+    keys = {
+      { "<Leader>1", "<cmd>BufferLineGoToBuffer 1<CR>", desc = "Go to buffer 1" },
+      { "<Leader>2", "<cmd>BufferLineGoToBuffer 2<CR>", desc = "Go to buffer 2" },
+      { "<Leader>3", "<cmd>BufferLineGoToBuffer 3<CR>", desc = "Go to buffer 3" },
+      { "<Leader>4", "<cmd>BufferLineGoToBuffer 4<CR>", desc = "Go to buffer 4" },
+      { "<Leader>5", "<cmd>BufferLineGoToBuffer 5<CR>", desc = "Go to buffer 5" },
+      { "<Leader>6", "<cmd>BufferLineGoToBuffer 6<CR>", desc = "Go to buffer 6" },
+      { "<Leader>7", "<cmd>BufferLineGoToBuffer 7<CR>", desc = "Go to buffer 7" },
+      { "<Leader>8", "<cmd>BufferLineGoToBuffer 8<CR>", desc = "Go to buffer 8" },
+      { "<Leader>9", "<cmd>BufferLineGoToBuffer 9<CR>", desc = "Go to buffer 9" },
+    }
+  }
 }
