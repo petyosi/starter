8c64011bdc374219eb9fa49a4fe33fdb47d6fd13
Petyo Ivanov <underlog@gmail.com>
2023-05-09 14:06:43 +0300

    css formatting seems to work

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,15 @@
 return {
   -- add gruvbox
   { "sainnhe/everforest" },
+  { "ellisonleao/gruvbox.nvim", priority = 1000, opts = {
+    transparent_mode = true,
+  } },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "everforest",
+      colorscheme = "gruvbox",
     },
   },
 }
