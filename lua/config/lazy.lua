be1f412137f93b4b8beeb983494d9499b663991f
Petyo Ivanov <underlog@gmail.com>
2023-05-23 09:35:06 +0300

    env highlight

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,10 +1,19 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
+
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = ".env.*",
+  callback = function()
+    vim.bo.filetype = "sh"
+  end,
+})
 
 require("lazy").setup({
   spec = {
