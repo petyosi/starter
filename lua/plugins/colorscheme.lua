83d86e1745189cdb5483a15c262463b287aeab5b
Petyo Ivanov <underlog@gmail.com>
2023-05-29 10:25:29 +0300

    monokai pro

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,19 @@
 return {
   -- add gruvbox
   { "sainnhe/everforest" },
+  {"loctvl842/monokai-pro.nvim", opts = {
+    transparent_background = true,
+    devicons = true,
+    filter = "spectrum",
+    background_clear = {
+    -- "float_win",
+    "toggleterm",
+    "telescope",
+    "which-key",
+    "renamer",
+      "neo-tree",
+    },
+  }},
   { "ellisonleao/gruvbox.nvim", priority = 1000, opts = {
     transparent_mode = true,
   } },
@@ -8,7 +21,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "monokai-pro",
     },
   },
 }
