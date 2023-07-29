96a9a6966daaf4e5f4adc47f0945ca3b00f799fa
Petyo Ivanov <underlog@gmail.com>
2023-07-29 08:52:37 +0300

    Try catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,4 @@
 return {
-  -- add gruvbox
   { "sainnhe/everforest" },
   {
     "loctvl842/monokai-pro.nvim",
@@ -16,15 +15,37 @@ return {
         "neo-tree",
       },
     },
-  },
-  { "ellisonleao/gruvbox.nvim", priority = 1000, opts = {
-    transparent_mode = true,
-  } },
-
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
+    {
+      "catppuccin/nvim",
+      name = "catppuccin",
+      opts = {
+        flavour = "frappe",
+        transparent_background = true,
+        term_colors = false,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          neotree = true,
+          telescope = true,
+          notify = true,
+          mini = true,
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+      },
+    },
+    {
+      "ellisonleao/gruvbox.nvim",
+      priority = 1000,
+      opts = {
+        transparent_mode = true,
+        contrast = "soft",
+      },
+    },
+    {
+      "LazyVim/LazyVim",
+      opts = {
+        colorscheme = "catppuccin",
+      },
     },
   },
 }
