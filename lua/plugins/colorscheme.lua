c2bd0cd12ecdf9deb799bd4f6db98ea2c1e802e9
Petyo Ivanov <underlog@gmail.com>
2024-01-24 13:55:02 +0200

    Back to the gruv

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -51,9 +51,14 @@ return {
       },
     },
     {
+      "folke/tokyonight.nvim",
+      lazy = true,
+      opts = { style = "night" },
+    },
+    {
       "LazyVim/LazyVim",
       opts = {
-        colorscheme = "catppuccin",
+        colorscheme = "gruvbox",
       },
     },
   },
