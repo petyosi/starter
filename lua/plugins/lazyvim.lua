09a861f99dbaea183a49378b8ef4cec9cc3174d2
Petyo Ivanov <underlog@gmail.com>
2025-02-09 08:44:57 +0200

    tokyonight again

--- lua/plugins/lazyvim.lua
+++ lua/plugins/lazyvim.lua
@@ -2,7 +2,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      colorscheme = "tokyonight-night",
       kind_filter = {
         typescript = {
           "Variable",
