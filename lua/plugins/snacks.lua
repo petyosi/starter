252ad7b7c0ba0a792d23c29614c828cef43eaf44
Petyo Ivanov <underlog@gmail.com>
2025-11-05 11:24:02 +0200

    Try snacks gh

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -5,6 +5,10 @@ return {
     { "<leader>p", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
     { "<leader>o", function() Snacks.picker.buffers() end, desc = "Buffers" },
     { "<A-j>", function() Snacks.explorer() end, desc = "Open explorer" },
+    { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
+    { "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
+    { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
+    { "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub Pull Requests (all)" },
   },
   ---@type snacks.Config
   opts = {
